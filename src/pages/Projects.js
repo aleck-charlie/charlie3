757e186861f839736420ef94b40498106c4f9df8
charliecharlie <aleckcharlie@gmail.com>
2021-09-25 14:05:39 -0500

    restructuring project cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "../assets/css/Projects.css";
-import $ from jQuery;
+
 
 export default function Project() {
-
   return (
-    <div>
-
-    </div>
-  );
+    <div></div>
+  )
 }
