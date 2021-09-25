a203bde7ae861f619ed68c8f90ea309cbb95ea5d
charliecharlie <aleckcharlie@gmail.com>
2021-09-25 17:37:00 -0500

    updated home card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import '../assets/css/Home.css';
-import img from '../assets/img/icecream1.jpg'
+import React from "react";
+import "../assets/css/Home.css";
+import img from "../assets/img/icecream1.jpg";
 
-export default function Home(){
-
-    return (
-        <section>
-            <div className="main-card">
-                <div className="nested">
-                <div className="home-img"><img src={img} alt="bowl of pink ice cream" /></div>
-                <div className="home-text">Peanut butter cups brownie bits peanut butter dark chocolate marshmallows. Peanut butter brownie bits chocolate chip cookie dough whipped cream, rainbow sprinkles strawberry strawberry mint chocolate chip coffee chocolate chips brownie bits. Dark chocolate chocolate syrup rocky road, whipped cream chocolate syrup kitkat strawberry.</div>
-            </div>
-            </div>
-        </section>
-    )
+export default function Home() {
+  return (
+    <section>
+      <div className="main-card">
+        <div className="nested">
+          <div className="home-img">
+            <img src={img} alt="bowl of pink ice cream" />
+          </div>
+          <div className="home-text">
+            As a full stack web developer and creative I strive to create
+            projects that are engaging, responsive and exploratory-- in work and
+            in life.
+            <p /> From art school and film, to welding and working aboard
+            commercial fishing vessels in Alaska, and now in front-to-backend
+            development, I thrive in collaboration and creative problem solving.
+            
+            <p />My developer toolkit includes HTML, CSS, Javascript, JQuery, AJAX,
+            Node.js, and experience in SQL and MongoDB databases, and REST and
+            Graphql API's. 
+            
+            <p />Moving forward, I'm learning to build projects with Python,
+            AWS, Java and C#, with an interest in focusing in on more in-depth
+            CSS and SCSS capabilities.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
