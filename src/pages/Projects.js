e4412b40ae7d7088033d8cb56dc38a6489ccf87e
charliecharlie <aleckcharlie@gmail.com>
2021-09-25 13:33:57 -0500

    importing jquery

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../assets/css/Projects.css";
+import $ from jQuery;
 
 export default function Project() {
 
   return (
     <div>
-      
+
     </div>
   );
 }
