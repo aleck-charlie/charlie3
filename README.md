49e8e11c4bdbce5ea1eb94dd08264a0421310179
Charlie Aleck <aleckcharlie@gmail.com>
2021-11-05 11:59:48 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# charlie3
-Portfolio v.3
+# Charlie Aleck Portfolio v3
+
+## Description
+This single-page React app is the third version of my portfolio, showcasing recent projects, links to my Github, LinkedIn and CV, and a short bio about myself. 
+
+## Table of Contents
+1. [Installation](#installation)
+2. [License](#license)
+3. [Questions](#questions)
+
+## Installation
+
+Github repository is located at (https://github.com/aleck-charlie/charlie3). The deployed application is located at (https://charlie3.herokuapp.com/)
+
+## License
+MIT
+
+## Questions
+Find me on Github at https://github.com/aleck-charlie. Contact me at aleckcharlie@gmail.com.
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <div className="main-card">
         <div className="nested">
           <p className="home-text">
-          Full-stack Web Developer using a background as a mixed media artist, writer and event coordinator to build polished applications from front to back. Recent graduate of Northwestern’s Full Stack program with a focus on CSS, Javascript, jQuery, React.js, SQL, MongoDB, and GraphQL. Currently deepen skills in Python, AWS and C#, as well as SASS, SCSS and UI/UX design. Known as an effective communicator and team leader in high-stress situations. Lifelong learner that thrives in collaborative problem-solving, and aims to create engaging and impactful applications with accessible mobile-first design. 
+          Full-stack Web Developer using a varied background as a mixed media artist, writer, commercial fisherman, welder and event coordinator to build engaging and polished projects from front to back. Recent graduate of Northwestern’s Full Stack program with a focus on CSS, Javascript, jQuery, React.js, SQL, MongoDB, and GraphQL. Currently deepen skills in Python, AWS and C#, as well as SASS, SCSS and UI/UX design. Known as an effective communicator and team leader in high-stress situations. Lifelong learner that thrives in collaborative problem-solving, and aims to create impactful web applications with accessible mobile-first design. 
           </p>
         </div>
       </div>
