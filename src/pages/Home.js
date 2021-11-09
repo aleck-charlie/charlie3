80a661d5a247fd0f62c8d60f344c7e29eb91e3d1
Charlie Aleck <aleckcharlie@gmail.com>
2021-11-09 15:54:42 -0600

    summary

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,20 +11,12 @@ export default function Home() {
             <img src={img} alt="bowl of pink ice cream" className="splash-img"/>
           </div>
           <div className="home-text">
-            As a full stack web developer and creative I strive to create
-            projects that are engaging, responsive and exploratory-- in work and
-            in life.
-            <p /> From art school and film, to welding and working aboard
-            commercial fishing vessels in Alaska, and now in front-to-backend
-            development, I thrive in collaboration and creative problem solving.
-            
-            <p />My developer toolkit includes HTML, CSS, Javascript, JQuery, AJAX,
-            Node.js, and experience in SQL and MongoDB databases, and REST and
-            Graphql API's. 
-            
-            <p />Moving forward, I'm learning to build projects with Python,
-            AWS, Java and C#, with an interest in focusing in on more in-depth
-            CSS and SCSS capabilities.
+          Full-stack Web Developer using a varied background as a mixed media artist, writer,
+commercial fisherman, welder and event coordinator to build engaging and polished projects
+from front to back. Recent graduate of Northwestern’s Full Stack program. Known as an
+effective communicator and team leader in high-stress situations. Lifelong learner that thrives in
+collaborative problem-solving, and aims to create impactful web applications with accessible
+mobile-first design.
           </div>
         </div>
       </div>
