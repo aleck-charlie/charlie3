ff313f3f64e1453b7c0bb5d9990fcfcc5acd3e35
Charlie Aleck <aleckcharlie@gmail.com>
2021-11-09 22:06:24 -0600

    installed react pdf

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Document } from 'react-pdf'
 import "../assets/css/Home.css";
 
 
 export default function About() {
+  
   return (
     <section>
       <div className="main-card">
         <div className="nested">
           <p className="home-text">
+          <Document file="..." />
+          
+
           </p>
         </div>
       </div>
