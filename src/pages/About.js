a19a44a36328c6888de21c2f80f97b0ab32be444
Charlie Aleck <aleckcharlie@gmail.com>
2021-11-13 21:23:43 -0600

    bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,12 +8,27 @@ export default function About() {
         <div className="nested">
           <p className="home-text">
             Charlie Aleck (they/them) is a queer Indigenous Seattle-born and
-            Chicago-based artist, writer, and web developer. 
-            <br/>
-            <br/>
-            They create their best work while tinkering with new tools and ideas, finding shared
-            stories within collaborative project building and trusting their
-            instinct.  
+            Chicago-based artist, writer, and web developer with a background
+            that encompasses illustration, community art, event coordinating,
+            welding, and commercial fishing in Alaska.
+            <br />
+            <br />
+            A recent graduate of Northwestern University's Full Stack
+            program, they continue to create applications with a focus on clean
+            coding practices and user-intuitive design. Their portfolio
+            currently highlights projects built with React.js, Node.js,
+            Javascript, CSS3, HTML5, jQuery, Express.js, SQL, MongoDB, and
+            GraphQL. They are currently deepening their skills in Python, SCSS, UI/UX design and more.
+            <br />
+            <br />
+            As an enrolled member of Tla-o-qui-aht First Nations, they are actively involved in Indigenous language revitalization and seek to uplift Indigenous culture and communities in art, writing and mutual aid.
+            <br />
+            <br />
+            They find they create their best work while tinkering with new
+            tools, finding shared stories and trusting their instinct.
+            <br />
+            <br />
+            Resume | Contact
           </p>
         </div>
       </div>
