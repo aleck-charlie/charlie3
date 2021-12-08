88df840d208c504a84cb319f322dc1ce4912594d
Charlie Aleck <aleckcharlie@gmail.com>
2021-12-08 11:15:33 -0600

    created gallery array

---
+++ src/data/gallery.js
@@ -0,0 +1,11 @@
+const gallery =[
+    {
+    id: 1,
+    src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1638981592/Illustrations/87245430_848308655596329_5850555029024806565_n_18078253180168260_rx1uux.jpg",
+},
+{   id:2,
+    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+}
+];
+
+export default gallery; 
