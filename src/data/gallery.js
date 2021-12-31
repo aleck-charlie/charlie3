07b929007fe1f06a4a20bf6ad4217fa8251395de
Charlie Aleck <aleckcharlie@gmail.com>
2021-12-30 18:49:18 -0600

    added img

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -6,100 +6,105 @@ const gallery = [
   },
   {
     id: 2,
+    src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640911594/Untitled_Artwork_1_fqiday.jpg",
+    tag: "skiff bone"
+  },
+  {
+    id: 3,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640892668/bottles_xirlwo.jpg",
     tag: "bottles"
   },
   {
-    id: 3,
+    id: 4,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640893359/Wintery_x5lh1b.png",
     tag: "winter chemex"
   },
   {
-    id: 4,
+    id: 5,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1638981592/Illustrations/87245430_848308655596329_5850555029024806565_n_18078253180168260_rx1uux.jpg",
     tag: "blue overalls"
   },
   {
-    id: 5,
+    id: 6,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640892515/tuktuk_aytb85.png",
     tag: "tuktuk"
   },
   {
-    id: 6,
+    id: 7,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640892494/87414580_2669906489753177_789622672249308261_n_17842460132022201_gwsaai.jpg",
     tag: "yellow dress"
   },
   {
-    id: 7,
+    id: 8,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640893551/ClozaEstate_m0movy.png",
     tag: "cloza"
   },
   {
-    id: 8,
+    id: 9,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640895820/IMG_9351_g81hmw.jpg"
   },
   { 
-    id: 9,
+    id: 10,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640893557/Arnold_Paz__jififs.png",
     tag: "arnold paz"
   },
   { 
-    id: 10,
+    id: 11,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640893707/Overseer_rdkpkz.png",
     tag: "beyond"
   },
   {
-    id: 11,
+    id: 12,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640892505/whale_esa6vz.jpg",
     tag: "whale constellation"
   },
   {
-    id: 111,
+    id: 13,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640897132/IMG_0789_difgci.jpg",
     tag: "longroom nights"
   },
   {
-    id: 121,
+    id: 14,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640897132/IMG_2906_zai2oi.jpg",
     tag: "snow walk"
   },
   {
-    id: 12,
+    id: 15,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640896322/IMG_9355_sh3vlu.jpg",
     tag: "three headed dog"
   },
   { 
-    id: 13,
+    id: 16,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640894902/IMG_9346_xncayr.jpg",
     tag: "wolf"
   },
   {
-    id: 14,
+    id: 17,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640895148/IMG_9348_llqjpn.jpg",
     tag: "dragonfly ovoid"
   },
   {
-    id: 15,
+    id: 18,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640895149/IMG_9347_jxmk3h.jpg",
     tag: "whale suwa"
   },
   {
-    id: 16,
+    id: 19,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640894449/IMG_9342_phajex.jpg",
     tag: "fresh nerves"
   },
   {
-    id: 17,
+    id: 20,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640893715/redjeans_ubwhfx.jpg",
     tag: "red jeans"
   },
   {
-    id: 171,
+    id: 21,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640897132/IMG_5669_q1vrc7.jpg",
     tag: "knot"
   },
   {
-    id: 18,
+    id: 22,
     src: "https://res.cloudinary.com/dvhx34ziw/image/upload/v1640896552/IMG_9356_ausmf7.jpg",
     tag: "leg doodle"
   }
